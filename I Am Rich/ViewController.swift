c13c1c25dbf2b8849e1f8b45a83834fe8b57a9d9
Khaya Mathebula <khayaprom@gmail.com>
2017-11-14 19:14:56 +0200

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Khaya Mathebula on 2017/11/14.
+//  Copyright © 2017 Aeon Source. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
